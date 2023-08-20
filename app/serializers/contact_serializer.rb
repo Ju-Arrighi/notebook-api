58fbec46f7fbb63c32d93f1574f0466832565807
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-08-20 13:30:06 +0200

    Visualizing associated fields.

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -4,4 +4,10 @@ class ContactSerializer < ActiveModel::Serializer
   belongs_to :kind
   has_many :phones
   has_one :address
+
+  # def attributes(*args)
+  #   h = super(*args)
+  #   h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+  #   # pt-BR ---> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+  # end
 end
