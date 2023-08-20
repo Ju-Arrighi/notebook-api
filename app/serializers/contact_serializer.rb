69caabb9d06757b4b0845bc7fd5fffea7a3c87ef
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-08-20 14:23:38 +0200

    Adjust link related.

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -6,7 +6,7 @@ class ContactSerializer < ActiveModel::Serializer
   has_one :address
 
   belongs_to :kind do
-    link(:kind) { kind_url(object.kind.id) }
+    link(:related) { kind_url(object.kind.id) }
   end
 
   link(:self) { contact_path(object.id) }
