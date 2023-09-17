9cbf070a89112059fd91336ad80cc01f5aeaf912
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-09-17 15:01:32 +0200

    Devise Token Auth.

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -2,8 +2,8 @@ class KindsController < ApplicationController
   # include ActionController::HttpAuthentication::Basic::ControllerMethods
   # http_basic_authenticate_with name: 'juliana', password: 'secret', except: [:index]
   # TOKEN = 'secret123'
-  include ActionController::HttpAuthentication::Token::ControllerMethods
-  before_action :authenticate
+  # include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :authenticate_user!
   before_action :set_kind, only: %i[show update destroy]
 
   # GET /kinds
@@ -59,15 +59,15 @@ class KindsController < ApplicationController
     params.require(:kind).permit(:description)
   end
 
-  def authenticate
-    authenticate_or_request_with_http_token do |token, options|
-      hmac_secret = 'my$ecretK3y'
-      JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
-      # Compare tokens in a time constant manner to avoid timing attack
-      # ActiveSupport::SecurityUtils.secure_compare(
-      #   ::Digest::SHA256.hexdigest(token),
-      #   ::Digest::SHA256.hexdigest(TOKEN)
-      # )
-    end
-  end
+  # def authenticate
+  #   authenticate_or_request_with_http_token do |token, options|
+  #     hmac_secret = 'my$ecretK3y'
+  #     JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+  #     # Compare tokens in a time constant manner to avoid timing attack
+  #     # ActiveSupport::SecurityUtils.secure_compare(
+  #     #   ::Digest::SHA256.hexdigest(token),
+  #     #   ::Digest::SHA256.hexdigest(TOKEN)
+  #     # )
+  #   end
+  # end
 end
