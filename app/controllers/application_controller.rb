44a4ade5aec40dd1ce8e28dd4dfb8346fc729e40
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-09-18 18:37:09 +0200

    Accept x content-type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # before_action :ensure_json_request
+
+  # def ensure_json_request
+  #   if request.headers['Accept'] =~ /vnd\.api\+json/
+  #     render nothing: true, status: 406
+  #   else
+  #     unless request.get?
+  #       return if request.headers['Content-type'] =~ /vnd\.api\+json/
+
+  #       render nothing: true, status: 415
+  #     end
+  #   end
+  # end
 end
